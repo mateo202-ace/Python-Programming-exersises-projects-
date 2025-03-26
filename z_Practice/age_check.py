5177b78b63dbbdfdfa6ee141453d60cbc059b8a0
Juan Mateo <jmateo5916@gmail.com>
2025-03-26 11:15:03 -0400

    committing other practice projects

---
+++ z_Practice/age_check.py
@@ -0,0 +1,10 @@
+users_age = int(input("How old are you: "))
+
+if users_age < 13:
+    print("Child")
+elif users_age <= 19:
+    print("Teenager")
+else:
+    print("Adult")
+
+
---
+++ z_Practice/even_or_odd.py
@@ -0,0 +1,5 @@
+for n in range(1, 21):
+    if n % 2 ==  0:
+        print("Even")
+    else:
+        print("Odd")
