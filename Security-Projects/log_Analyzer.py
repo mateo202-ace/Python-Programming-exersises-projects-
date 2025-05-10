52a7bdb9c8df8abc65d138c42fd12b19918a55e8
Juan Mateo <jmateo5916@gmail.com>
2025-05-09 20:33:33 -0400

    Refactor log analyzer to use dynamic log file path and improve failed login tracking

--- Security-Projects/log_Analyzer.py
+++ Security-Projects/log_Analyzer.py
@@ -1,34 +1,46 @@
 import os
 import re
+from pathlib import Path
 
 
 # defining the dummy log files
-INPUT_LOG_FILE = "/Users/juanmateo/Desktop/Security-Projects/Security-Projects/dummy_logs/auth.log"
+# INPUT_LOG_FILE = "/Users/juanmateo/Desktop/Security-Projects/Security-Projects/dummy_logs/auth.log"
+
+script_dir = Path(__file__).resolve().parent
+log_file_path = script_dir / "dummy_logs" / "auth.log"
+
+failed_attempts_user = {}
+failed_attempts_ip = {}
 
 
 failed_attempts = []
-with open(INPUT_LOG_FILE, "r") as file:
+with log_file_path.open("r") as file:
     for line in file:
         if "Failed password" in line:
-            user_match = re.search(r"(?<=user\s)(\S+)", line) # Extract the username
-            ip_match = re.search(r"(?<=from\s)(\S+)", line) # Extract the IP address
+            # Extract the user namees
+            match_user = re.search(r"(?<=user\s)(\S+)", line)
+            if match_user:
+                user = match_user.group(0)
+                if user in failed_attempts_user:
+                    failed_attempts_user[user] += 1
+                else:
+                    failed_attempts_user[user] = 1
 
-            if user_match and ip_match:
-                failed_attempts.append({
-                    "User" : user_match.group(0),
-                    "IP" : ip_match.group(0)
-                })
-           
-            
-    
+            # Extract the IP address
+            match_ip = re.search(r"(?<=from\s)(\S+)", line)
+            if match_ip:
+                ip = match_ip.group(0)
+                if ip in failed_attempts_ip:
+                    failed_attempts_ip[ip] += 1
+                else:
+                    failed_attempts_ip[ip] = 1
+
 # Print the failed attempts
-print("Failed login attempts:")
-for attempt in failed_attempts:
-    print(f"User: {attempt['User']} | IP: {attempt['IP']}")
+print("Failed login attempts by user:")
+for user, count in failed_attempts_user.items():
+    print(f"{user}: {count} failed attempts")
 
+print("\nFailed login attempts by IP:")
+for ip, count in failed_attempts_ip.items():
+    print(f"{ip}: {count} failed attempts")
 
-
-
-
-
-   
